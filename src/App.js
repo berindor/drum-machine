3815f55466f3ffe6477208e082fb2d1bd3d427b6
Beringer Dorottya <beringerdorottya@gmail.com>
2023-11-23 16:59:47 +0100

    first steps of app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playedSound: ''
+    }
+  }
+
+  render() {
+    return (
+      <div id="drum-machine">
+        <div id="pad-container">
+          <div id="Q" className="drum-pad">Q</div>
+          <div id="W" className="drum-pad">W</div>
+          <div id="E" className="drum-pad">E</div>
+          <div id="A" className="drum-pad">A</div>
+          <div id="S" className="drum-pad">S</div>
+          <div id="D" className="drum-pad">D</div>
+          <div id="Z" className="drum-pad">Z</div>
+          <div id="X" className="drum-pad">X</div>
+          <div id="C" className="drum-pad">C</div>
+        </div>
+        <div id="display"></div>
+      </div>
+    )
+  }
 }
 
 export default App;
